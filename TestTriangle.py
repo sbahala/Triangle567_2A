5c714427ab9bdd9b8325eacfebd3bd781bde5ed3
sbahala <sbahala@stevens.edu>
2022-09-21 10:59:49 +0530

    Added test cases and rectified error

--- TestTriangle.py
+++ TestTriangle.py
@@ -22,9 +22,30 @@ class TestTriangles(unittest.TestCase):
 
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
+    
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')    
+        
+    def testScaleneTriangles(self): 
+        self.assertEqual(classifyTriangle(2,3,4),'Scalene','2,3,4 should be Scalene')
+    
+    def testIsocelesTriangle(self): 
+        self.assertEqual(classifyTriangle(4,4,5),'Isoceles','4,4,5 should be Isoceles')
+    
+    def testNotATriangle(self): 
+        self.assertEqual(classifyTriangle(1,1,4),'NotATriangle','1,1,4 is not a triangle')    
+    
+    def testIfValidIntegers(self): 
+         self.assertEqual(classifyTriangle(3.4,4.5,6.7),'InvalidInput','3.4,4.5,6.7 are not integers')
+    
+    def testValidInputA(self): 
+        self.assertEqual(classifyTriangle(0,-2,-1),'InvalidInput','0,-2,-1 should be invalid input')
+        
+    def testValidInputB(self): 
+        self.assertEqual(classifyTriangle('-sv',-3,4.0),'InvalidInput','-sv,-3,4.0 should be invalid sides')
+        
+    def testTriangleSides(self): 
+        self.assertEqual(classifyTriangle(300,499,500),'InvalidInput','300,499,500 should be invalid sides')
 
 if __name__ == '__main__':
     print('Running unit tests')
